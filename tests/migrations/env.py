9ba386a79c410214b4164eb98a2e87db8cf8794b
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2024-11-08 09:12:01 +0300

    add tests

--- tests/migrations/env.py
+++ tests/migrations/env.py
@@ -16,6 +16,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 from db.models import Base
 target_metadata = Base.metadata
 # target_metadata = None
---
+++ tests/test_handlers.py
@@ -0,0 +1,23 @@
+import json
+
+
+async def test_create_user(client, get_user_from_database):
+    user_data = {
+        "name": "Slavick",
+        "surname": "Snegirev",
+        "email": "test@test.com"
+    }
+    resp = client.post("/user/", data=json.dumps(user_data))
+    data_from_resp = resp.json()
+    assert resp.status_code == 200
+    assert data_from_resp["name"] == user_data["name"]
+    assert data_from_resp["surname"] == user_data["surname"]
+    assert data_from_resp["email"] == user_data["email"]
+    user_from_db = await get_user_from_database(data_from_resp["user_id"])
+    assert len(user_from_db) == 1
+    user_from_db = dict(user_from_db[0])
+    assert user_from_db["name"] == user_data["name"]
+    assert user_from_db["surname"] == user_data["surname"]
+    assert user_from_db["email"] == user_data["email"]
+    assert user_from_db["is_active"] is True
+    assert str(user_from_db["user_id"]) == data_from_resp["user_id"]
